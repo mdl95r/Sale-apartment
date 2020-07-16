bf1dd81eb86a7f42e94a567a2f32ae5ea2733543
Artyom Bogomolov <awesomemdl95@gmail.com>
2020-07-16 22:55:52 +0300

    update js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,32 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const burgerBtn = $('#burger-btn');
-    const menuList = $('.header-nav__list');
-    
-    burgerBtn.on('click', function() {
-        $(this).toggleClass('active');
-        if (burgerBtn.hasClass('active')) {
-            $(menuList).css('display', 'block')
-            $('.header-nav').css('left', '0');
+    const burgerBtn = document.getElementById('burger-btn'),
+           menuList = document.querySelector('.header-nav__list'),
+             header = document.querySelector('.header'),
+            headNav = document.querySelector('.header-nav'),
+              upBtn = document.querySelector('.up-btn-container'),
+           cardsBtn = document.querySelectorAll('.cards-item__status-btn');
+
+    burgerBtn.addEventListener('click',function(){
+        this.classList.toggle('active');
+        if (burgerBtn.classList.contains('active')) {
+            menuList.style.display = 'block';
+            headNav.style.left = '0';
         } else {
-            $('.header-nav').css('left', '');
+            headNav.style.left = '';
             setTimeout(function(){
-                $(menuList).removeAttr('style');  
-                $('.header-nav').removeAttr('style');
+                menuList.removeAttribute('style');
+                headNav.removeAttribute('style');
+                burgerBtn.removeAttribute('class');
             },300);
         }
     });
 
     function resize() {
-        let w = $(window).width();
+        let w = window.innerWidth;
     
         return function() {
-            if( w != $( window ).width() ){
-                $('.header-nav').css('left', '');
-                setTimeout(function(){
-                    $(burgerBtn).removeClass('active');
-                    $(menuList).css('display', '');
+            if( w != window.innerWidth ){
+                headNav.style.left =  '';
+                setTimeout(function() {
+                    burgerBtn.classList.remove('active');
+                    menuList.style.display =  '';
                 },300)
-                w = $( window ).width();
+                w = window.innerWidth;
             
                 delete w;
               }
@@ -35,44 +40,56 @@ document.addEventListener('DOMContentLoaded', function() {
     
     windowRes = resize();
     
-    $(window).on('resize', function() {
+    window.addEventListener('resize', function(){
+        let w  = window.innerWidth;
+
+        if (w < 769) {
+        	upBtn.style.display = "none";
+        }
+        
         windowRes();
     });
-
-
-    $(window).scroll(function(){
-        let height = $(document).scrollTop();
-        let w 	   = $(window).width();
+  
+    window.addEventListener('scroll', function(){
+        let height = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        let w 	   =  window.innerWidth;
         if (w > 1024) {
             if ( height >= 200 ) {
-                $(".up-btn-container").css("display", "block");
-            } else {
-            if ( height < 200 ) 
-                $(".up-btn-container").css("display", "none");
-            };
+                upBtn.style.display = "block";
+            }
+            if ( height < 200 ) {
+                upBtn.style.display = "";
+            }
+        }
+
+        if (w <= 768) {
+            if (height >= 300) {
+                header.classList.add('header-fixed');
+            }
+
+            if (height == 0) {
+                header.classList.remove('header-fixed');
+            }
         }
     });
-
-	$(".up-btn-container").on("click", function() {
-        // let w 				= 	$(window).width();
-
-        $("html, body").animate({ scrollTop: 0 }, 500);
-		//  	menu 			= 	$(".menu"),
-		// 	displayBlock 	= 	$(".menu").css("display", "none");	
-		// if(w < 769) {
-        // 	if ( menu != displayBlock) {
-		// 		$(".menu").css("display", "none");
-		// 		$(".header").removeClass("down");
-		// 	}
-        // } else {
-        // 	if (w > 770) {
-        // 		$(".menu").removeAttr("style");
-        // 	}
-        // };
-    });
     
-    $('.cards-item__status-btn').on('click', function() {
-        $(this).removeClass('cards-item__status-btn--free')
-        .addClass('cards-item__status-btn--booked').text('Забронировано');
+    upBtn.addEventListener('click', function() {
+        backToTop();
+    });
+
+
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+          window.scrollBy(0, -20);
+          setTimeout(backToTop, 0);
+        }
+    }
+
+    cardsBtn.forEach(function(button) {
+        button.addEventListener('click', function() {
+            button.classList.remove('cards-item__status-btn--free');
+            button.classList.add('cards-item__status-btn--booked');
+            button.innerHTML = 'Забронировано';
+        });
     });
 });
